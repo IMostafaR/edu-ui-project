2280c19e8d0c9ced19c4a07b7d1ebcc2da99b6f6
Mostafa Ragab <imostaf.ragab@gmail.com>
2024-12-15 23:53:45 +0200

    edit scroll

--- src/components/scroll/ScrollToTop.jsx
+++ src/components/scroll/ScrollToTop.jsx
@@ -6,7 +6,7 @@ const ScrollToTop = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+      window.scrollTo({ top: 0, left: 0, behavior: "instant" });
     };
 
     handleScroll();
