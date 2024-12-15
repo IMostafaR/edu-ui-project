50a8c94d3ae13f4255f36be190e8fd5af9d85c18
Mostafa Ragab <imostaf.ragab@gmail.com>
2024-12-15 21:18:14 +0200

    scrollToTop component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import Footer from "../shared/footer/Footer.jsx";
 import Navbar from "../shared/navbar/Navbar.jsx";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "./scroll/ScrollToTop.jsx";
 
 const Layout = () => {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
